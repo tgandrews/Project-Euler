c3f9aacfd2a65455f8c3d949a169561dfe3b8eac
Tom Andrews <git@tomandrews.co.uk>
2010-05-26 21:26:02 +0100

    Completed problem 6. Can be done purely mathematically, but I have brute forced it. 0.5x10e-5 seconds is fast enough for me.

---
+++ Problem 6/Difference between sum of square and square of sum.py
@@ -0,0 +1,31 @@
+# Difference between sum of square and square of sum
+# (1^2 + 2^2) - (1 + 2)^2
+
+import time;
+
+def Sum(maxVal):
+    resultSquares = 0;
+    resultSum =  0;
+
+    i = 0;
+    while i <= maxVal:
+        resultSquares += (i * i);
+        resultSum += i;
+        i = i + 1;
+
+    print 'Sum: ' + str(resultSum);
+    print 'Sum of squares: ' + str(resultSquares);
+
+    resultSumSquared = resultSum * resultSum;
+    result = resultSumSquared - resultSquares;
+    if result < 0:
+        result = result * -1;
+    return result;
+
+print 'Working on first 100 natural numbers'
+start = time.time();
+result = Sum(100);
+end = time.time();
+print 'Result: ' + str(result);
+print 'Took: ' + str(end - start) + ' seconds';
+
